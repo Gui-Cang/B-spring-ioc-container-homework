a400cc348036af17b4542e250c6f645254479832
Gui-Cang <821192514@qq.com>
2020-11-10 01:55:10 +0800

    [dingke] feat: problem 2, 2nd way.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,10 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
 @RestController
+@Scope(SCOPE_SINGLETON)
 public class GreetingController {
 
     private final GreetingService greetingService;
@@ -16,6 +20,7 @@ public class GreetingController {
 
     @GetMapping("/greet")
     public String greet() {
+        System.out.println("abc");
         return greetingService.sayHi();
     }
 

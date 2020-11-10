4d849d7f07dac8f7fa6df9e94e10129813caf3bd
Gui-Cang <821192514@qq.com>
2020-11-10 23:45:32 +0800

    [dingke] feat: finish problem3, 3rd way just override afterPropertiesSet and setApplicationContext.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,14 +1,30 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Bar {
+public class Bar implements ApplicationContextAware, InitializingBean {
 
     private Foo foo;
 
-    public Bar(Foo foo) {
-        this.foo = foo;
+    private ApplicationContext context;
+
+    public Foo getFoo() {
+        return foo;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        foo = context.getBean(Foo.class);
+    }
+
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
     }
 
     public void hi() {
